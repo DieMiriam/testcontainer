0e9ea96c6c51b64203d9ddb047d8cda6c555e994
DieMiriam <miriam.dermaku@gmail.com>
2024-09-10 06:54:01 +0000

    Get 1.

---
+++ index.js
@@ -0,0 +1,42 @@
+// import
+const express =require('express');
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+
+// Listener
+app.listen(
+    PORT,
+    () => console.log(`Server ready at http://localhost:${PORT}`) 
+);
+
+
+// Endpoint GET
+// app.get('/' , (req,res) => {
+//     res.status(200).send({
+//         product: "T-Shirt",
+//         size: "L",
+//         prize: 10.00,
+//         hasLogo: false
+//     })
+// });
+
+
+// Endpoint GET
+app.get('/' , (req,res) => {
+    res.status(200).send(`
+        <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meine Seite</title>
+</head>
+<body>
+    <h1>Coole Sache</h1>
+    <p>Das ist echt toll!</p>
+</body>
+</html>
+          `)
+});
